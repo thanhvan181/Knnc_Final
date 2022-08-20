b0cc0b8db01644dad076c575be7fe8bb4ad516ed
hieuminh <nmhieu04091999@gmail.com>
2022-08-20 20:56:19 +0700

    config : router

--- src/knnc_frontend/src/App.tsx
+++ src/knnc_frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, NavLink, Navigate } from "react-router-dom";
+import { Routes, Route, NavLink, Navigate, Link } from "react-router-dom";
 import ClientLayout from "./layouts/client/ClientLayout";
 import AdminLayout from "./layouts/admin/AdminLayout";
 import Home from "./pages/client/Home/Home";
@@ -13,9 +13,9 @@ function App() {
           <Route index element={<Home />} />
           <Route path="/home" element={<Home />} />
         </Route>
-        <Route path="/admin" element={<AdminLayout />}>
+        <Route path="admin" element={<AdminLayout />}>
           <Route index element={<TestAdmin />} />
-          <Route path="/" element={<TestAdmin />} />
+          <Route path="test" element={<TestAdmin />}/>
         </Route>
       </Routes>
     </div>
--- src/knnc_frontend/src/layouts/client/Header/Header.tsx
+++ src/knnc_frontend/src/layouts/client/Header/Header.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 type Props = {};
 
 const Header = (props: Props) => {
-  return <div>Header</div>;
+  return <div><Link to={'/admin'}>Admin</Link></div>;
 };
 
 export default Header;
